a88a49989cf5e18556f1292738b9823af795c8e6
白空 <472597160@qq.com>
2024-05-04 23:44:56 +0800

    perf(initCharts): 把ResizeObserver和chart提到export外面
    用map把echart的resize和dispose方法存起来，触发resize让Observer通过查询dom来执行对应的resize方法，销毁指令时同理

--- src/mixmin/initCharts.js
+++ src/mixmin/initCharts.js
@@ -1,31 +1,37 @@
 // 初始化echarts
 import { init } from 'echarts/core';
+import { debounce } from '@/utils/util';
 
+const map = new WeakMap();
+const observer = new ResizeObserver(
+  debounce(
+    entries => {
+      for (const entry of entries) {
+        const handler = map.get(entry.target).resize;
+        handler && handler();
+      }
+    },
+    200,
+    true
+  )
+);
 export default {
-  data() {
-    return {
-      chart: undefined,
-      observer: undefined
-    };
-  },
   directives: {
     initCharts: {
-      inserted(el, { value }, vnode) {
-        const that = vnode.context;
+      inserted(el, { value }) {
         //生成echarts图表
-        that.chart = init(el);
-        value && that.chart.setOption(value);
+        const chart = init(el);
+        value && chart.setOption(value);
+        map.set(el, { resize: chart.resize, dispose: chart.dispose });
         //监听dom大小变化
-        const debounceFn = that.$utils.debounce(that.chart.resize, 200, true);
-        that.observer = new ResizeObserver(debounceFn);
-        that.observer.observe(el);
+        observer.observe(el);
       },
-      unbind(el, binding, vnode) {
-        const that = vnode.context;
+      unbind(el) {
         //取消监听
-        that.observer.disconnect();
+        observer.disconnect();
         //销毁图表实例
-        that.chart.dispose();
+        const handler = map.get(el).dispose;
+        handler && handler();
       }
     }
   }
